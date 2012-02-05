fc79e5ae36035555b3cd272c103e717aad404bf3
Charles Sprayberry <cspray@gmail.com>
2012-02-05 01:54:19 -0500

    ADD GetHub/Entities/UserStub basic just to get tests running

---
+++ src/GetHub/Entities/UserStub.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @file
+ * @brief Holds a file for a class representing a variety of github user objects
+ * that only represents a small selection of data about that user.
+ */
+
+namespace GetHub\Entities;
+
+class UserStub extends \GetHub\Entities\GravatarUser {
+
+    /**
+     * @return string A URL for the user's github profile
+     */
+    public function getProfileUrl() {
+
+    }
+
+}
