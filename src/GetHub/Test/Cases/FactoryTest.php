8c7b980616bb87778b89ed1432a36ab4bcff6480
Charles Sprayberry <cspray@gmail.com>
2012-02-05 15:12:06 -0500

    UPD GetHub/Test/Cases/FactoryTest adding first basic test

--- src/GetHub/Test/Cases/FactoryTest.php
+++ src/GetHub/Test/Cases/FactoryTest.php
@@ -9,18 +9,30 @@ namespace GetHub\Test\Cases;
 
 class FactoryTest extends \PHPUnit_Framework_TestCase {
 
+    /**
+     * @brief The single Factory to produce objects from in this test; ensures that
+     * we have a quicker test because we only have to create the Factory once.
+     *
+     * @property $Factory GetHub.Test.Helpers.Factory
+     */
     protected $Factory;
 
+    /**
+     * @brief Ensures that \a $this->Factory has a valid object in it.
+     */
     public function setUp() {
         if (!isset($this->Factory)) {
             $this->Factory = new \GetHub\Test\Helpers\Factory();
         }
     }
 
+    /**
+     * @brief Ensures that we can create a DooHickey with some data and that the
+     * appropriate DooHickey gets made.
+     */
     public function testCreatingBasicValidDooHickey() {
-
-
-
+        $DooHickey = $this->Factory->createObject();
+        $this->assertTrue($DooHickey instanceof \GetHub\Test\Helpers\DooHickey);
     }
 
 }
