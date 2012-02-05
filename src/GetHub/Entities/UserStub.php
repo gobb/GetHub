1e1011c9e5264d8a14e4fa288bed5a4e5d4c36b9
Charles Sprayberry <cspray@gmail.com>
2012-02-05 10:36:43 -0500

    UPD GetHub/Entities/UserStub passing tests

--- src/GetHub/Entities/UserStub.php
+++ src/GetHub/Entities/UserStub.php
@@ -11,10 +11,31 @@ namespace GetHub\Entities;
 class UserStub extends \GetHub\Entities\GravatarUser {
 
     /**
+     * @brief The numeric github ID for this user
+     *
+     * @property $id int
+     */
+    protected $id = 0;
+
+    /**
+     * @brief The login for this user
+     *
+     * @property $name string
+     */
+    protected $name = '';
+
+    /**
+     * @brief The github API URL for this object
+     *
+     * @property $apiUrl string
+     */
+    protected $apiUrl = 'https://api.github.com/';
+
+    /**
      * @return string A URL for the user's github profile
      */
     public function getProfileUrl() {
-
+        return 'http://github.com/' . $this->name;
     }
 
 }
