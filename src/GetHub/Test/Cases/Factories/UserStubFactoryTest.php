5f04f72d8b8a444639a5b705060c71fe2cb55906
Charles Sprayberry <cspray@gmail.com>
2012-02-05 17:54:51 -0500

    ADD GetHub/Test/Cases/Factories/UserStubFactoryTest first test

---
+++ src/GetHub/Test/Cases/Factories/UserStubFactoryTest.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * @file
+ * @brief Holds a PHPUnit test case to confirm the functionality of GetHub.Entities.UserStub
+ */
+
+namespace GetHub\Test\Cases\Factories;
+
+class UserStubFactoryTest extends \PHPUnit_Framework_TestCase {
+
+    /**
+     * @property $Factory GetHub.Factories.UserStubFactory
+     */
+    protected $Factory;
+
+    /**
+     * @brief Ensures that we have a UserStubFactory to create GetHub.Entities.UserStub
+     * objects with.
+     */
+    public function setUp() {
+        if (!isset($this->Factory)) {
+            $this->Factory = new \GetHub\Factories\UserStubFactory();
+        }
+    }
+
+    /**
+     * @brief Tests that the factory is producing a valid object correctly
+     */
+    public function testUserStubFactoryCreatingValidEntity() {
+        $data = array(
+            'id' => 1,
+            'login' => 'cspray',
+            'gratavar_id' => '#1234',
+            'url' => 'https://api.github.com/users/cspray'
+        );
+        $Stub = $this->Factory->createObject($data);
+        $this->assertTrue($Stub instanceof \GetHub\Entities\UserStub);
+        $this->assertSame(1, $Stub->id);
+        $this->assertSame('cspray', $Stub->name);
+        $this->assertSame('#1234', $Stub->gravatarId);
+        $this->assertSame('https://api.github.com/users/cspray', $Stub->apiUrl);
+    }
+
+}
