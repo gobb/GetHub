bf2dfe567fa2ae58c894e30de3b7a9ff6f5e6460
Charles Sprayberry <cspray@gmail.com>
2012-02-05 23:50:29 -0500

    ADD GetHub/Entities/User

---
+++ src/GetHub/Entities/User.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @file
+ * @brief Holds a class that stores all the information publicly available about
+ * a given github user.
+ */
+
+namespace GetHub\Entities;
+
+class User extends \GetHub\Entities\UserStub {
+
+
+
+}
