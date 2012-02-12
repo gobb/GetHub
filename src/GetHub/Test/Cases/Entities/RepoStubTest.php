c4b5272ae5203b310ca0c8670892b82b30275b00
Charles Sprayberry <cspray@gmail.com>
2012-02-12 15:37:16 -0500

    UPD fixing some errors in repoStub test, changing values to not be default

--- src/GetHub/Test/Cases/Entities/RepoStubTest.php
+++ src/GetHub/Test/Cases/Entities/RepoStubTest.php
@@ -28,14 +28,14 @@ class RepoStubTest extends \PHPUnit_Framework_TestCase {
             'owner' => $RepoOwner,
             'name' => 'GetHub',
             'description' => 'This is the description of the awesomeness that is GetHub.',
-            'homepage' => null,
+            'homepage' => 'http://github.com/GetHub.github.com',
             'language' => 'PHP',
-            'isPrivate' => false,
-            'isFork' => false,
+            'isPrivate' => true,
+            'isFork' => true,
             'numberOfTimesForked' => 10,
             'numberOfWatchers' => 5,
-            'masterBranch' => 'master',
-            'openIssues' => 10,
+            'masterBranch' => '1.0.0',
+            'numberOfOpenIssues' => 15,
             'pushedAt' => '2012-01-01T14:35:20',
             'createdAt' => '2011-12-31T01:49:00'
         );
@@ -46,16 +46,16 @@ class RepoStubTest extends \PHPUnit_Framework_TestCase {
         $this->assertSame($RepoOwner, $RepoStub->owner);
         $this->assertSame('cspray', $RepoStub->owner->name);
         $this->assertSame(1, $RepoStub->owner->id);
-        $this->assertSame('GetHub', $RepoOwner->name);
+        $this->assertSame('GetHub', $RepoStub->name);
         $this->assertSame('This is the description of the awesomeness that is GetHub.', $RepoStub->description);
         $this->assertNull($RepoStub->homepage);
         $this->assertSame('PHP', $RepoStub->language);
-        $this->assertFalse($RepoStub->isPrivate, 'The repository is private but is not expected to be.');
-        $this->assertFalse($RepoStub->isFork, 'The repository is a fork but is not expected to be.');
+        $this->assertTrue($RepoStub->isPrivate, 'The repository is not private but is expected to be.');
+        $this->assertTrue($RepoStub->isFork, 'The repository is a not a fork but is expected to be.');
         $this->assertSame(10, $RepoStub->numberOfTimesForked);
         $this->assertSame(5, $RepoStub->numberOfWatchers);
-        $this->assertSame('master', $RepoStub->masterBranch);
-        $this->assertSame(10, $RepoStub->openIssues);
+        $this->assertSame('1.0.0', $RepoStub->masterBranch);
+        $this->assertSame(15, $RepoStub->numberOfOpenIssues);
         $this->assertSame('2012-01-01T14:35:20', $RepoStub->pushedAt);
         $this->assertSame('2011-12-31T01:49:00', $RepoStub->createdAt);
     }
